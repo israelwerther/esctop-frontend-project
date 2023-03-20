c9a34947365caa4bd9b5f9a2ee95904965420a89
israelwerther <israelwerther48@outlook.com>
2023-03-20 17:56:44 -0300

    reorganiza pastas de components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './template/header/header.component';
+import { SidenavComponent } from './template/sidenav/sidenav.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
